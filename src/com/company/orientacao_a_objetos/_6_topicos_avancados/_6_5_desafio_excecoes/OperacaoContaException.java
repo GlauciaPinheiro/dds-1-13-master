eb6883b88c94f95e98c845780a7271879e27ae24
glaucia do socorro <glaucia.coreia@gmail.com>
2022-06-19 22:10:55 -0300

    Initial commit

---
+++ src/com/company/orientacao_a_objetos/_6_topicos_avancados/_6_5_desafio_excecoes/OperacaoContaException.java
@@ -0,0 +1,8 @@
+package com.company.orientacao_a_objetos._6_topicos_avancados._6_5_desafio_excecoes;
+
+public class OperacaoContaException extends Exception{
+    public OperacaoContaException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/_5_21_classes_abstratas/Produto.java
@@ -0,0 +1,11 @@
+package com.company.orientacao_a_objetos.orientacao_a_objetos_parte2._5_21_classes_abstratas;
+
+public abstract class Produto {
+    String Descricao;
+    public abstract void imprimirDescricao();
+    public String getDescricao(){
+        return Descricao;
+
+    }
+
+}
